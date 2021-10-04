fc2ad5e497b1e9d3ecebd0a529a3aca4f6277bd7
Mohamed Fouad <mohamed@fouad.me>
2021-10-04 19:08:20 +0200

    Update dependencies

---
+++ carthage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# carthage.sh
+# Usage example: ./carthage.sh build --platform iOS
+# Workaround for Couldn't IRGen Expression: see https://steipete.com/posts/couldnt-irgen-expression/
+
+set -euo pipefail
+
+xcconfig=$(mktemp /tmp/static.xcconfig.XXXXXX)
+trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
+
+echo 'OTHER_SWIFT_FLAGS = -Xfrontend -no-serialize-debugging-options' >> $xcconfig
+echo 'SWIFT_SERIALIZE_DEBUGGING_OPTIONS = NO' >> $xcconfig
+echo 'BUILD_LIBRARY_FOR_DISTRIBUTION=YES' >> $xcconfig
+export XCODE_XCCONFIG_FILE="$xcconfig"
+carthage "$@"
